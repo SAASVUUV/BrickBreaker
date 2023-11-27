77017924037533e0d09383b686c05549fb5c54f3
SAASVUUV <133169815+SAASVUUV@users.noreply.github.com>
2023-11-27 18:59:34 -0300

    Add files via upload

---
+++ skins.py
@@ -0,0 +1,8 @@
+from PPlay.window import*
+from PPlay.sprite import*
+from PPlay.gameimage import*
+
+def skins(janela, teclado, cursor):
+    janela.set_background_color([0,0,0])
+    while True:
+        janela.update()
